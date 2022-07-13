3ee4cb83e0f54e10248fa639a12f78d723081e6f
Nataliia Chala <hatalochka.rv@gmail.com>
2022-07-13 11:51:06 +0100

    updated link to github repository

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 import "./footer.css";
 export default function Footer(){
     return(
-        <div className="fixed-bottom text-center text-light"><a href="/" target="_blank" rel="noreferrer">Open-source code</a> by Nataliia Chala</div>
+        <div className="fixed-bottom text-center text-light"><a href="https://github.com/natalie-0073/weather-react-app.git" target="_blank" rel="noreferrer">Open-source code</a> by Nataliia Chala</div>
     );
 }
